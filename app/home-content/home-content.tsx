d60fda6f3ea4f92c26cda055a89db454aca59102
Dmitrii <dimterion59@gmail.com>
2026-08-02 11:20:28 +0200

    adjust layout

--- app/home-content/home-content.tsx
+++
@@ -1,5 +0,0 @@
-export function HomeContent() {
-  return (
-    <main className="flex items-center justify-center pt-16 pb-4">Profile</main>
-  );
-}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { HomeContent } from "../home-content/home-content";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Profile" },
-    { name: "description", content: "Home content." },
+    { name: "description", content: "Profile site" },
   ];
 }
 
 export default function Home() {
-  return <HomeContent />;
+  return <>Profile</>;
 }
