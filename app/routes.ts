254c9ccc9222024326d965c7625fd1feb4b3c95c
Dmitrii <dimterion59@gmail.com>
2026-08-03 11:08:01 +0200

    add initial routes structure

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,8 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home/index.tsx"),
+  route("projects", "./routes/projects/index.tsx"),
+  route("posts", "./routes/posts/index.tsx"),
   route("about", "./routes/about/index.tsx"),
+  route("contact", "./routes/contact/index.tsx"),
 ] satisfies RouteConfig;
