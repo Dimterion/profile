3db5876fb9a4895597053e1ed1704ee4c434efe1
Dmitrii <dimterion59@gmail.com>
2026-08-08 10:13:00 +0200

    add initial projects page structure

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -2,8 +2,19 @@ import type { Project } from "~/types";
 import { siteText } from "../../../data/content";
 
 export default function ProjectsPage() {
-  const project: Project = siteText.en.projects.items[0];
-  console.log(project);
+  const projects: Project[] = siteText.en.projects.items;
 
-  return <>Projects page</>;
+  return (
+    <>
+      <h2 className="font-bold">Projects</h2>
+      <section className="flex flex-wrap gap-4">
+        {projects.map((project) => (
+          <article key={project.id} className="border">
+            <h3 className="font-semibold">{project.title}</h3>
+            <p>{project.description}</p>
+          </article>
+        ))}
+      </section>
+    </>
+  );
 }
