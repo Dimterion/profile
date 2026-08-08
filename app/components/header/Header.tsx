a620effd7d5ac2abb0c2424f8eb6e6b774b16a03
Dmitrii <dimterion59@gmail.com>
2026-08-08 09:57:37 +0200

    adjust header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -12,7 +12,7 @@ const diamondClip = {
 
 export default function Header({ showPortrait = false }: HeaderProps) {
   return (
-    <header className="flex w-full items-center justify-between p-2 md:p-4">
+    <header className="fixed inset-x-0 top-0 z-50 flex items-center justify-between bg-gray-200 p-2 md:p-4">
       {showPortrait ? (
         <div
           className="relative h-20 w-20 shrink-0 md:h-30 md:w-30"
