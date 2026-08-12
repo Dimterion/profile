0f949fd2763df583bf8bc9b40c1b57a9fbb6b7c8
Dmitrii <dimterion59@gmail.com>
2026-08-12 15:42:46 +0200

    adjust ProjectDetailsPage content

--- app/routes/projects/details.tsx
+++ app/routes/projects/details.tsx
@@ -21,19 +21,29 @@ export default function ProjectDetailsPage({
   const { project } = loaderData;
 
   return (
-    <article className="space-y-2">
+    <section className="space-y-4">
       <Link to="/projects" className="flex w-fit items-center gap-2 border p-2">
         <ArrowLeftIcon /> Back to projects
       </Link>
-      <h1>{project.title}</h1>
-      <img src={project.image.link} alt={project.image.description} />
-      <div className="space-x-2">
-        {project.stack.map((item) => (
-          <span className="border px-2 py-1">{item}</span>
-        ))}
-      </div>
-      <p>{project.description}</p>
-      <div className="flex flex-col gap-2">
+      <article className="grid items-start gap-8 md:grid-cols-2">
+        <div>
+          <img
+            src={project.image.link}
+            alt={project.image.description}
+            className="w-full shadow-md"
+          />
+        </div>
+        <div>
+          <h1 className="mb-4 text-3xl font-bold">{project.title}</h1>
+          <div className="mb-4 space-x-2">
+            {project.stack.map((item) => (
+              <span className="border px-2 py-1">{item}</span>
+            ))}
+          </div>
+          <p className="mb-6">{project.description}</p>
+        </div>
+      </article>
+      <div className="flex gap-2">
         {project.links.map((link) => (
           <a
             target="_blank"
@@ -45,6 +55,6 @@ export default function ProjectDetailsPage({
           </a>
         ))}
       </div>
-    </article>
+    </section>
   );
 }
