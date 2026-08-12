7d3202df15846f36b2c5ead0a405f90c82aa76f4
Dmitrii <dimterion59@gmail.com>
2026-08-12 15:59:08 +0200

    add pagination to ProjectsPage

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Route } from "./+types/index";
 import { siteText } from "../../data/content";
 import ProjectCard from "~/components/projectCard/ProjectCard";
@@ -11,14 +12,38 @@ export async function loader({}: Route.LoaderArgs) {
 export default function ProjectsPage({ loaderData }: Route.ComponentProps) {
   const { projects } = loaderData;
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const projectsPerPage = 2;
+
+  const totalPages = Math.ceil(projects.length / projectsPerPage);
+
+  const indexOfLast = currentPage * projectsPerPage;
+  const indexOfFirst = indexOfLast - projectsPerPage;
+  const currentProjects = projects.slice(indexOfFirst, indexOfLast);
+
+  const renderPagination = () => (
+    <div>
+      {Array.from({ length: totalPages }, (_, idx) => (
+        <button
+          key={idx + 1}
+          className={`m-4 cursor-pointer px-3 py-1 ${currentPage === idx + 1 ? "bg-gray-600 text-white" : "bg-gray-400"}`}
+          onClick={() => setCurrentPage(idx + 1)}
+        >
+          {idx + 1}
+        </button>
+      ))}
+    </div>
+  );
+
   return (
     <>
       <h2 className="font-bold">Projects</h2>
       <section className="my-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {projects.map((project) => (
+        {currentProjects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </section>
+      {totalPages > 1 && renderPagination()}
     </>
   );
 }
