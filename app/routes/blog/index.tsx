81e07686bfa4ee2a297daf2161b3783ea30de599
Dmitrii <dimterion59@gmail.com>
2026-08-17 09:57:14 +0200

    add postsMeat loader

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,3 +1,25 @@
-export default function BlogPage() {
+import type { Route } from "./+types";
+import { postsMeta } from "~/data/blog/posts-meta";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Blog | Dimterion" },
+    {
+      name: "description",
+      content: "Dimterion's blog.",
+    },
+  ];
+}
+
+export async function loader({}: Route.LoaderArgs) {
+  return {
+    posts: postsMeta.en,
+  };
+}
+
+export default function BlogPage({ loaderData }: Route.ComponentProps) {
+  const { posts } = loaderData;
+  console.log(posts);
+
   return <>Blog page</>;
 }
