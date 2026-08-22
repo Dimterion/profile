c52feae4fe9935490977a417357c506871428cb6
Dmitrii <dimterion59@gmail.com>
2026-08-22 09:00:16 +0200

    adjust Header-TI comp

--- app/components/header/Header-TI.tsx
+++ app/components/header/Header-TI.tsx
@@ -1,32 +1,35 @@
 import { NavLink } from "react-router";
 import profileImg from "../../assets/images/profile_img.png";
 import { HouseIcon } from "../icons";
+import type { HeaderProps } from "~/types";
 
 const diamondClip = {
   clipPath: "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)",
 };
 
-export default function Header() {
+export default function Header({ showPortrait = false }: HeaderProps) {
   return (
-    <header className="flex items-center gap-1">
-      <div
-        className="relative h-20 w-20 shrink-0 "
-        aria-label="Profile image"
-      >
-        <div className="absolute inset-0 bg-current" style={diamondClip} />
+    <header className="fixed inset-x-0 top-0 z-50 flex items-center gap-1 p-1">
+      {showPortrait && (
+        <div className="relative h-20 w-20 shrink-0" aria-label="Profile image">
+          <div className="absolute inset-0 bg-current" style={diamondClip} />
 
-        <div className="absolute inset-px overflow-hidden" style={diamondClip}>
-          <img
-            src={profileImg}
-            alt="Profile"
-            className="h-full w-full object-cover"
-          />
+          <div
+            className="absolute inset-px overflow-hidden"
+            style={diamondClip}
+          >
+            <img
+              src={profileImg}
+              alt="Profile"
+              className="h-full w-full object-cover"
+            />
+          </div>
         </div>
-      </div>
+      )}
 
       <NavLink
         to="/"
-        className="inline-flex h-7 w-7 rotate-45 items-center justify-center border"
+        className={`inline-flex h-7 w-7 rotate-45 items-center justify-center border ${!showPortrait && "m-1"}`}
       >
         <span className="inline-flex -rotate-45">
           <HouseIcon />
