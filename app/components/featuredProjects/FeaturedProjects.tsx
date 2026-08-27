214d1f80fef9d0769cc694a04b1fddd70f3ccf31
Dmitrii <dimterion59@gmail.com>
2026-08-27 09:53:04 +0200

    adjust ProjectCard comp

--- app/components/featuredProjects/FeaturedProjects.tsx
+++ app/components/featuredProjects/FeaturedProjects.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import type { FeaturedProjectsProps } from "~/types";
 import ProjectCard from "../ProjectCard/ProjectCard";
 import CornerFrame from "../CornerFrame/CornerFrame";
@@ -11,13 +12,21 @@ export default function FeaturedProjects({
     .slice(0, count);
 
   return (
-    <CornerFrame className="bg-blue w-full max-w-96 border p-2 md:max-w-lg md:p-4 lg:max-w-2xl xl:max-w-full">
-      <h2 className="mb-4 text-center text-lg font-bold md:text-xl">Work</h2>
+    <CornerFrame className="bg-blue flex w-full max-w-96 flex-col gap-6 border p-2 md:max-w-lg md:p-4 lg:max-w-2xl xl:max-w-full">
+      <h2 className="text-center text-lg font-bold uppercase md:text-xl">
+        Work
+      </h2>
       <div className="grid gap-2 md:mx-2 md:mb-2 md:gap-6 lg:grid-cols-2">
         {featured.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
+      <Link
+        className="bg-dark-blue hover:text-gold hover:border-gold mx-auto w-fit border px-4 pt-0.5 pb-1 text-sm uppercase transition"
+        to="/projects"
+      >
+        All projects
+      </Link>
     </CornerFrame>
   );
 }
--- app/components/projectCard/ProjectCard.tsx
+++ app/components/projectCard/ProjectCard.tsx
@@ -6,17 +6,19 @@ export default function ProjectCard({ project }: { project: Project }) {
   return (
     <Link
       to={`/projects/${project.slug}`}
-      className="project-card-enter bg-dark-blue flex transform flex-col border transition duration-100 hover:scale-[1.01]"
+      className="project-card-enter group bg-dark-blue flex transform flex-col border transition duration-100"
     >
+      <div className="overflow-hidden">
+        <img
+          src={project.image.link}
+          alt={project.image.description}
+          loading="lazy"
+          className="w-full object-cover transition-transform duration-300 group-hover:scale-105"
+        />
+      </div>
       <h3 className="p-2 text-center font-semibold md:text-lg">
         {project.title}
       </h3>
-      <img
-        src={project.image.link}
-        alt={project.image.description}
-        loading="lazy"
-        className="w-full object-cover"
-      />
       <div className="flex flex-1 items-center">
         <p className="min-w-0 flex-1 p-2">{project.description}</p>
         <ArrowRightIcon className="mr-2 h-5 w-5 shrink-0" />
