b343bcad44ce299f09debe6b97aae512c52cbbb5
Dmitrii <dimterion59@gmail.com>
2026-08-27 10:06:34 +0200

    adjust AboutPreview comp

--- app/components/aboutPreview/AboutPreview.tsx
+++ app/components/aboutPreview/AboutPreview.tsx
@@ -3,33 +3,44 @@ import CornerFrame from "../CornerFrame/CornerFrame";
 
 export default function AboutPreview() {
   return (
-    <CornerFrame className="bg-blue w-full max-w-96 border p-2 md:max-w-lg md:p-4 lg:max-w-2xl xl:max-w-full">
-      <div>
-        <h2 className="mb-4 text-center text-lg font-bold md:text-xl">
-          Profile
-        </h2>
-        <p>
-          I contribute to GitHub daily, publish on Medium weekly, track
-          everything I read, watch, and play, and make time every day to learn
-          and write.
-        </p>
+    <CornerFrame className="bg-blue flex w-full max-w-96 flex-col gap-6 border p-2 md:max-w-lg md:p-4 lg:max-w-2xl xl:max-w-full">
+      <h2 className="text-center text-lg font-bold uppercase md:text-xl">
+        Profile
+      </h2>
+      <div className="grid gap-6 lg:grid-cols-2 lg:gap-2">
+        <div className="bg-dark-blue border p-4">
+          <h3 className="mb-4 text-center text-lg font-bold md:text-xl">
+            Overview
+          </h3>
+          <p>
+            I contribute to GitHub daily, publish on Medium weekly, track
+            everything I read, watch, and play, and make time every day to learn
+            and write.
+          </p>
+        </div>
+        <div className="bg-dark-blue border p-4">
+          <h3 className="mb-4 text-center text-lg font-bold md:text-xl">
+            Highlights
+          </h3>
+          <ul className="mb-4 list-none space-y-2">
+            <li className="relative pl-4 before:absolute before:top-2.25 before:left-0 before:h-2 before:w-2 before:-rotate-45 before:border-0 before:bg-white before:content-['']">
+              Daily GitHub contribution streak: 4+ years.
+            </li>
+            <li className="relative pl-4 before:absolute before:top-2.25 before:left-0 before:h-2 before:w-2 before:-rotate-45 before:border-0 before:bg-white before:content-['']">
+              Weekly Medium posts every Friday: 4+ years.
+            </li>
+            <li className="relative pl-4 before:absolute before:top-2.25 before:left-0 before:h-2 before:w-2 before:-rotate-45 before:border-0 before:bg-white before:content-['']">
+              Duolingo streak: 4,400+ days.
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <h3 className="mb-4 text-center font-semibold md:text-lg">
-          Highlights
-        </h3>
-        <ul className="mb-4">
-          <li>Daily GitHub contribution streak: 4+ years.</li>
-          <li>Weekly Medium posts every Friday: 4+ years.</li>
-          <li>Duolingo streak: 4,400+ days.</li>
-        </ul>
-        <Link
-          to="/about"
-          className="mt-2 inline-block border px-2 py-0.5 text-sm"
-        >
-          About
-        </Link>
-      </div>
+      <Link
+        className="bg-dark-blue hover:text-gold hover:border-gold mx-auto w-fit border px-4 pt-0.5 pb-1 text-sm uppercase transition"
+        to="/about"
+      >
+        About
+      </Link>
     </CornerFrame>
   );
 }
