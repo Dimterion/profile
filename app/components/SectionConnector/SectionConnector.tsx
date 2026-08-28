82bb3bba0e34235c23b449fc7b48fb3463434b76
Dmitrii <dimterion59@gmail.com>
2026-08-28 10:58:57 +0200

    adjust Hero comp

--- app/components/SectionConnector/SectionConnector.tsx
+++ app/components/SectionConnector/SectionConnector.tsx
@@ -4,9 +4,9 @@ export default function SectionConnector() {
       aria-hidden="true"
       className="flex flex-col items-center justify-center"
     >
-      <div className="h-4 w-px bg-white" />
+      <div className="h-7 w-px bg-white" />
       <div className="h-2 w-2 rotate-45 border border-white bg-white" />
-      <div className="h-4 w-px bg-white" />
+      <div className="h-7 w-px bg-white" />
     </div>
   );
 }
--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -1,15 +1,34 @@
+import { Link } from "react-router";
 import CornerFrame from "../CornerFrame/CornerFrame";
+
+const links = [
+  { link: "projects", label: "Work" },
+  { link: "blog", label: "Posts" },
+  { link: "about", label: "About" },
+  { link: "contact", label: "Contact" },
+];
 
 export default function Hero() {
   return (
-    <CornerFrame className="bg-blue w-full max-w-96 border px-2 py-4 text-center md:max-w-lg md:p-20 lg:max-w-2xl xl:max-w-full">
-      <h1 className="mb-2 text-lg font-bold md:text-2xl">
+    <CornerFrame className="bg-blue w-full max-w-96 border px-2 py-4 text-center md:max-w-lg md:px-4 md:py-20 lg:max-w-2xl xl:max-w-full">
+      <h1 className="mb-7 text-lg font-bold md:text-2xl">
         Dmitrii | Dimterion
       </h1>
       <p>
         Technical consultant, product support specialist, and developer with 10+
         years of experience in the video game and financial industries.
       </p>
+      <section className="mt-10 grid gap-4 justify-self-center lg:grid-cols-4">
+        {links.map((link) => (
+          <Link
+            key={link.link}
+            to={link.link}
+            className="bg-dark-blue hover:border-gold hover:text-gold w-full min-w-32 border px-4 py-2 text-xs uppercase transition lg:w-fit"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </section>
     </CornerFrame>
   );
 }
